aa62fe6a0b6e30f03867e21c00c0d8fe50c04d6d
kurone-kito <krone@kit.black>
2022-10-13 21:53:47 +0900

    feat: added the toggle event handler

--- Assets/Scripts/MirrorSystem.cs
+++ Assets/Scripts/MirrorSystem.cs
@@ -1,8 +1,16 @@
 ﻿
 using UdonSharp;
+using UnityEngine;
 
 /// <summary>鏡のオンオフを管理するクラス。</summary>
 [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
 public class MirrorSystem : UdonSharpBehaviour
 {
+    /// <summary>
+    /// トグル スイッチの操作に対して呼び出す、コールバック。
+    /// </summary>
+    public void OnToggle()
+    {
+        Debug.Log("MirrorSystem::OnToggle");
+    }
 }

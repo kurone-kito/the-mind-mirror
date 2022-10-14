93d3c40e1f0b0b6a130e6bc22183b027cc5063b8
kurone-kito <krone@kit.black>
2022-10-14 22:08:33 +0900

    feat: added the feature that toggles the mirror

--- Assets/Scripts/MirrorSystem.cs
+++ Assets/Scripts/MirrorSystem.cs
@@ -1,16 +1,32 @@
 ﻿
 using UdonSharp;
 using UnityEngine;
+using UnityEngine.UI;
 
 /// <summary>鏡のオンオフを管理するクラス。</summary>
 [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
 public class MirrorSystem : UdonSharpBehaviour
 {
+#pragma warning disable IDE0044
+    /// <summary>鏡本体オブジェクト。</summary>
+    [SerializeField]
+    private GameObject body = null;
+
+    /// <summary>トグル スイッチ。</summary>
+    [SerializeField]
+    private Toggle toggle = null;
+#pragma warning restore IDE0044
+
     /// <summary>
     /// トグル スイッチの操作に対して呼び出す、コールバック。
     /// </summary>
     public void OnToggle()
     {
-        Debug.Log("MirrorSystem::OnToggle");
+        if (body == null)
+        {
+            Debug.LogWarning("鏡のオブジェクトが設定されていません。");
+            return;
+        }
+        body.SetActive(toggle != null && toggle.isOn);
     }
 }

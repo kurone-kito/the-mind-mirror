aa16299afd7e9254c208852586c39501153f05de
kurone-kito <krone@kit.black>
2022-11-12 16:34:54 +0900

    feat: added an empty event handler of the mind-cube generate button

---
+++ Assets/Scripts/CubeGenerateForm.cs
@@ -0,0 +1,17 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+
+/// <summary>マインドキューブ生成フォーム。</summary>
+[UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+
+public class CubeGenerateForm : UdonSharpBehaviour
+{
+    /// <summary>
+    /// 生成ボタンの操作に対して呼び出す、コールバック。
+    /// </summary>
+    public void OnClickGenerateButton()
+    {
+        Debug.Log("マインドキューブを生成します。");
+    }
+}

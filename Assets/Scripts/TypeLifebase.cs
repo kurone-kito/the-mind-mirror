64833575f4995f0a85a73e48aef815d3288852c0
kurone-kito <krone@kit.black>
2022-11-21 21:42:23 +0900

    feat: added the enum definitions of the Dantalion engine

---
+++ Assets/Scripts/TypeLifebase.cs
@@ -0,0 +1,24 @@
+/// <summary>根底となる人生観のタイプ定義。</summary>
+public enum TypeLifebase : byte
+{
+    /// <summary>何でも自主体験する自分でありたいタイプ。</summary>
+    Application,
+    /// <summary>とにかく行動第一な自分でありたいタイプ。</summary>
+    Association,
+    /// <summary>より完璧な自分を築き続ける自分でありたいタイプ。</summary>
+    Development,
+    /// <summary>ありのままの素直な自分でありたいタイプ。</summary>
+    Expression,
+    /// <summary>欲しいものを手元に集める自分でありたいタイプ。</summary>
+    Finance,
+    /// <summary>地に足のついた収集家な自分でありたいタイプ。</summary>
+    Investment,
+    /// <summary>集団の一員としての自分でありたいタイプ。</summary>
+    Organization,
+    /// <summary>先人の知恵から学ぶ自分でありたいタイプ。</summary>
+    Quest,
+    /// <summary>集団のリーダーな自分でありたいタイプ。</summary>
+    SelfMind,
+    /// <summary>一匹狼な自分でありたいタイプ。</summary>
+    SelfReliance,
+}

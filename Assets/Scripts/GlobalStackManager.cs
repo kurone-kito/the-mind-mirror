e1459c480a18446c5a6e80d6817674f8ad8fc3b7
kurone-kito <krone@kit.black>
2022-12-02 02:23:37 +0900

    feat: implemented the synced mind stack

---
+++ Assets/Scripts/GlobalStackManager.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using UdonSharp;
+using UnityEngine;
+
+/// <summary>
+/// グローバルにマインドキューブのスタックを管理するクラス。
+/// </summary>
+[UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
+public sealed class GlobalStackManager : Subject
+{
+    /// <summary>
+    /// コア オブジェクトの接続不備における、エラーメッセージ。
+    /// </summary>
+    private const string ERR_NO_CORE =
+        "コア オブジェクトへのリンクが設定されていません。";
+
+#pragma warning disable IDE0044
+    /// <summary>
+    /// マインドキューブをスタックできるコア オブジェクト。
+    /// </summary>
+    [SerializeField]
+    private MindStack root;
+#pragma warning restore IDE0044
+
+    /// <summary>マインドキューブのインデックス。</summary>
+    [NonSerialized, UdonSynced, FieldChangeCallback(nameof(Index))]
+    public sbyte index = -1;
+
+    /// <summary>
+    /// マインドキューブのインデックスを取得、または設定します。
+    /// </summary>
+    public sbyte Index
+    {
+        get => index;
+        set
+        {
+            index = value;
+            SendCustomEventDelayedFrames(nameof(Notify), 1);
+        }
+    }
+
+    /// <summary>オブザーバーを呼び出します。</summary>
+    protected override void Notify()
+    {
+        if (root == null)
+        {
+            Debug.LogWarning(ERR_NO_CORE);
+        }
+        else
+        {
+            root.Forbid = index >= 0;
+        }
+        base.Notify();
+    }
+}
--- Assets/Scripts/MindDetails.cs
+++ Assets/Scripts/MindDetails.cs
@@ -1,18 +1,32 @@
-using TMPro;
+﻿using TMPro;
 using UdonSharp;
 using UnityEngine;
 using UnityEngine.UI;
 
 /// <summary>マインドキューブの情報ビューア。</summary>
 [UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
-public sealed class MindDetails : UdonSharpBehaviour
+public sealed class MindDetails : Observer
 {
+    /// <summary>
+    /// グローバル スタックの接続不備における、エラーメッセージ。
+    /// </summary>
+    private const string WARN_NO_GLOBAL_MANAGER =
+        "グローバル スタックへのリンクが設定されていません。";
+
 #pragma warning disable IDE0044
 #pragma warning disable IDE0051
     /// <summary>本文ラベル。</summary>
     [SerializeField]
     private TextMeshPro details;
+#pragma warning restore IDE0051
 
+    /// <summary>
+    /// グローバルにマインドキューブのスタックを管理するクラス。
+    /// </summary>
+    [SerializeField]
+    private GlobalStackManager globalStackManager;
+
+#pragma warning disable IDE0051
     /// <summary>名前ラベル。</summary>
     [SerializeField]
     private Text nameLabel;
@@ -39,5 +53,19 @@ public sealed class MindDetails : UdonSharpBehaviour
     {
         paginationLabel.text = "Prev";
         Debug.Log("OnPushPrevious");
+    }
+
+    /// <summary>
+    /// サブジェクトからの呼び出しを受けた際に呼び出す、コールバック。
+    /// </summary>
+    public override void OnNotify()
+    {
+        base.OnNotify();
+        if (globalStackManager == null)
+        {
+            Debug.LogWarning(WARN_NO_GLOBAL_MANAGER);
+            return;
+        }
+        Debug.Log($"MindDetails::OnNotify: {globalStackManager.Index}");
     }
 }

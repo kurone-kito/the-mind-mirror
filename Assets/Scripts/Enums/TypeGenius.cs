081368f8cb0979f5978a00f6a2aa8de8be8a3456
kurone-kito <krone@kit.black>
2022-12-13 01:32:00 +0900

    feat: implemented the resources sentences

--- Assets/Scripts/Enums/TypeGenius.cs
+++ Assets/Scripts/Enums/TypeGenius.cs
@@ -1,10 +1,10 @@
 /// <summary>大まか素質タイプ定義。</summary>
 public enum TypeGenius : byte
 {
-    /// <summary>自己権威に重きを置く、素質タイプ。</summary>
+    /// <summary>アート脳タイプ: 自己権威に重きを置く、素質タイプ。</summary>
     Authority,
-    /// <summary>自己資産に重きを置く、素質タイプ。</summary>
+    /// <summary>理系脳タイプ: 自己資産に重きを置く、素質タイプ。</summary>
     Economically,
-    /// <summary>自己人格に重きを置く、素質タイプ。</summary>
+    /// <summary>文系脳タイプ: 自己人格に重きを置く、素質タイプ。</summary>
     Humanely,
 }
